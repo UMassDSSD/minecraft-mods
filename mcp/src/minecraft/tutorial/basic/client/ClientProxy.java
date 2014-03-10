cd3b8911aaa8ae0e5e8d96da857d403bc63027d4
Austin <awalker4@umassd.edu>
2014-03-10 00:18:30 -0400

    first commit

---
+++ mcp/src/minecraft/tutorial/basic/client/ClientProxy.java
@@ -0,0 +1,16 @@
+	
+
+    package tutorial.basic.client;
+     
+    import net.minecraftforge.client.MinecraftForgeClient;
+    import tutorial.basic.CommonProxy;
+     
+    public class ClientProxy extends CommonProxy {
+           
+            @Override
+            public void registerRenderers() {
+                    // This is for rendering entities and so forth later on
+            }
+           
+    }
+
